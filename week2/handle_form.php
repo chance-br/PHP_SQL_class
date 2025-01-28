af3f896761ef1bff35e602cba1a2ec088dc6c087
chance-br <testingfortt5593@gmail.com>
2025-01-28 16:54:00 -0700

    Add files via upload

---
+++ week2/handle_form.php
@@ -0,0 +1,17 @@
+<!doctype html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Your Feedback</title>
+    </head>
+    <body>
+        <?php
+        $title = $_POST['title'];
+        $last_name = $_POST['last_name'];
+        $response = $_POST['response'];
+        $comments = $_POST['comments'];
+
+        print "<p>Thank you, $title $last_name for your comments.</p><p>You stated that you found this example to be '$response' and added:<br>$comments</p>"
+        ?>
+    </body>
