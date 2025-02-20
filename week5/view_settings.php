913ec65ebe281e4ea06f1a046b59857762202b06
chance-br <testingfortt5593@gmail.com>
2025-02-19 21:33:02 -0700

    Add files via upload

---
+++ week5/view_settings.php
@@ -0,0 +1,29 @@
+<!doctype html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>View Your Settings</title>
+        <style type="text/css">
+        body {
+            <?php
+            if (isset($_COOKIE['font_size'])) {
+                print "\t\tfont-size:" . htmlentities($_COOKIE['font_size']) . ";\n";
+            } else {
+                print "\t\tfont-size: medium;";
+            }
+
+            if (isset($_COOKIE['font_color'])) {
+                print "\t\tcolor:" . htmlentities($_COOKIE['font_color']) . ";\n";
+            } else {
+                print "\t\tcolor: #000;";
+            }
+            ?>
+        }
+        </style>
+    </head>
+    <body>
+        <p><a href="customize.php">Customize Your Settings</a></p>
+        <p><a href="reset.php">Reset Your Settings</a></p>
+        <p>aaaaaaaaaaaaaaa</p>
+    </body>
