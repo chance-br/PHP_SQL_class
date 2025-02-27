6cde6b1b1038a06c39a11795b6d1704df111bf15
chance-br <testingfortt5593@gmail.com>
2025-02-27 12:13:44 -0700

    Add files via upload

--- week6/add_quote.php
+++ week6/add_quote.php
@@ -12,7 +12,7 @@
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             if (!empty($_POST['quote']) && $_POST['quote'] != 'Enter your quotation here.') {
                 if (is_writable($file)) {
-                    file_put_contents($file, $_POST['quote'] . PHP_EOL, FILE_APPEND);
+                    file_put_contents($file, $_POST['quote'] . PHP_EOL, FILE_APPEND | LOCK_EX);
                     print '<p>Your quotation has been stored.</p>';
                 } else {
                     print '<p style="color:red;">Your quotation could not be stored due to a system error.</p>';
