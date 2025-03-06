be59c59e4dfb8a858acc9af36b2600cf001f0c29
chance-br <testingfortt5593@gmail.com>
2025-03-05 17:39:38 -0700

    Add files via upload

---
+++ week7/mysqli_connect.php
@@ -0,0 +1,17 @@
+<!doctype html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Connect to MySQL</title>
+    </head>
+    <body>
+        <?php
+        if ($dbc = mysqli_connect('localhost', 'root', 'admin', 'myblog')) {
+            print '<p>Successfully connected to the database!</p>';
+            mysqli_close($dbc);
+        } else {
+            print '<p style="color:red;">Could not connect to the database.</p>';
+        }
+        ?>
+    </body>
